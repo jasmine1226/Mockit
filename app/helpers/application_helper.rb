5f3cd57ad92408884a44d858a1de1ff28f1e6bf1
Jasmine <jasmine1226@gmail.com>
2019-11-03 13:43:12 -0800

    current user helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 module ApplicationHelper
-    
+    def current_user
+        if session[:id]
+            if Interviewer.find_by_id(session[:id])
+                @current_user = Interviewer.find(session[:id])
+            else
+                @current_user = Interviewee.find(session[:id])
+            end
+        else
+            @current_user = nil
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  post '/logout' => 'sessions#destroy'
+  get '/logout' => 'sessions#destroy'
 
   get '/auth/facebook/callback' => 'sessions#create'
 
