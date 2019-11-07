b9d0475f6f679351cb5fcd3934a0ffd906a3d3b4
Jasmine <jasmine1226@gmail.com>
2019-11-06 21:04:13 -0800

    update views and forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
+    def logged_in?
+        session[:id]
+    end
 
+    def current_user?
+        if params[:account_type] == 'Interviewer'
+            if params[:id] == session[:id]
+                @interviewer = Interviewer.find(session[:id])
+            end
+        elsif params[:account_type] == 'Interviewee'
+            if params[:id] == session[:id]
+                @interviewee = Interviewee.find(session[:id])
+            end
+        end
+    end
 end
